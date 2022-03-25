84fa52b64661281b6cd5342f610bfa6962e343dc
LUIS FELIPE HERNANDEZ ALTAMIRA <luisfelipe.hernandez.altamira.contractor@bbva.com>
2022-03-25 17:21:25 -0600

    Add text description into the component card

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,16 +1,20 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { fadeIn } from 'src/app/animations/animation';
 import { Character } from 'src/app/models/character';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
+  animations: [
+    fadeIn
+  ]
 })
 export class CardComponent implements OnInit {
 
   @Input()
   character?: Character;
-  
+
   constructor() { }
 
   ngOnInit(): void {
